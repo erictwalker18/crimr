a8a81942f3fd446d3ec150beddae0fd75f5649ba
Charlie Imhoff <imhoffc@carleton.edu>
2014-10-09 15:50:38 -0500

    first pass at this webapp.py file

---
+++ Code/webapptest.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import cgi
+
+#UTILITY METHODS
+def cleanInput(str):
+	''' Removes any control characters that out HTML might be screwed up by
+	'''
+	charsToRemove = ';,\\/:\'"<>@'
+    for ch in charsToRemove:
+        str = str.replace(ch, '')
+    return str
+
+def getParametersFromFormOrDefaults():
+	''' This function will always return a py dictionary of the params we care about.
+		
+		It works by either filling in and cleaning these params from a CGI form, 
+		or if none exists it just goes ahead and defines them as a blank string, 
+		instead of nil (or whatever null value Python uses...)
+    '''
+	parameters = {'search':''}
+	form = cgi.FieldStorage()
+	if 'search' in form:
+		parameters['search'] = cleanInput(form['search'].value)
+	return parameters
+	
+	
+	
+#PAGE CONSTRUCTION	
+def main():
+	''' Gets the parameters from defaults or a cgi form, and prints the HTML page
+	'''
+	parameters = getParametersFromFormOrDefaults()
+	print 'Content-type: text/html\r\n\r\n',
+	print(getPageAsHTML(parameters['search']))
+
+def getPageAsHTML(searchString):
+	''' Constructs and returns an HTML formatted string that can be printed, and thus
+		fill the webpage with content!
+		
+		I thought about linking this to a 'template.html' flat file, but it made more
+		sense to me (at this instant) that that technique would be pretty poorly coupled
+		as it would require a format string against a file that we would read without
+		clear expectations about it's contents. It feels more safe to do it this way,
+		even though design and code files aren't supposed to be together.
+	'''
+	page = '<!DOCTYPE HTML>'
+	page += '''
+		<html>
+		<head>
+			<title>webapptest</title>
+		</head>
+
+		<body>
+			<h2>webapptest.py</h2>
+			
+			<!-- form -->
+			<form action="webapptest.py" method="get">
+				<p>Search Crimr:<input type="text" name="search" value="%s" /></p>
+				<p><input type="submit" value="Go" /></p>
+			</form>
+
+			<!-- results get popped in here -->
+			%s
+			
+			<!-- links -->
+
+		</body>
+		</html>
+		''' % (searchString, getSearchResultsAsHTML(searchString))
+		
+	return page
+		
+def getSearchResultsAsHTML(searchString):
+	''' Returns the search results form the PSQL database, formatted into an HTML String
+		Will be placed directly into the output String of 'getPageAsHTML(searchString)'
+	'''
+	#TODO : Implement
+	return '''<p>Search Results for : %s</p>''' % searchString
