3af54ad49dd0a85e9a2f016c31e248d443684ceb
Graham Earley <graham.p.earley@gmail.com>
2014-10-13 14:40:52 -0500

    Updated comments to more accurately reflect current state of the program.

--- Code/webapp.py
+++ Code/webapp.py
@@ -2,9 +2,9 @@
 
 '''
 	CRIMR
-	
+
 	webapp.py (phase_2)
-	
+
 	Charlie Imhoff,
 	Graham Earley,
 	Eric Walker
@@ -13,10 +13,8 @@
 '''
 	This file was made with the intention of searching the dataset, but upon further work
 	we realized that it was impractical to implement a total search right now.
-	
-	Because of this, the user experience now using a form to ask for an appeasement, before the
-	data is loaded. This is filed around as a 'search', and comments in this document refer to it
-	as such. But it is not a search, it's simply an activator for the form.
+
+	Because of this, the user experience currently only searches by District.
 '''
 
 import cgi
@@ -27,7 +25,7 @@ from CrimeDataFetcher import CrimeDataFetcher
 
 #UTILITY METHODS
 def cleanInput(str):
-	''' Removes any control characters that out HTML might be screwed up by
+	''' Removes any control characters that our HTML might be screwed up by
 	'''
 	charsToRemove = ';,\\/:\'"<>@'
 	for ch in charsToRemove:
@@ -36,9 +34,9 @@ def cleanInput(str):
 
 def getParametersFromFormOrDefaults():
 	''' This function will always return a py dictionary of the params we care about.
-		
-		It works by either filling in and cleaning these params from a CGI form, 
-		or if none exists it just goes ahead and defines them as a blank string, 
+
+		It works by either filling in and cleaning these params from a CGI form,
+		or if none exists it just goes ahead and defines them as a blank string,
 		instead of nil (or whatever null value Python uses...)
     '''
 	parameters = {'search':''}
@@ -49,10 +47,10 @@ def getParametersFromFormOrDefaults():
 	except Exception, e:
 		parameters = {'search':'nothing'}
 	return parameters
-	
-	
-	
-#PAGE CONSTRUCTION	
+
+
+
+#PAGE CONSTRUCTION
 def main():
 	''' Gets the parameters from defaults or a cgi form, and prints the HTML page
 	'''
@@ -63,26 +61,26 @@ def main():
 def getPageAsHTML(searchString):
 	''' Constructs and returns an HTML formatted string that can be printed, and thus
 		fill the webpage with content!
-		
+
 		I thought about linking this to a 'template.html' flat file, but it made more
 		sense to me (at this instant) that that technique would be pretty poorly coupled
 		as it would require a format string against a file that we would read without
 		clear expectations about it's contents. It feels more safe to do it this way,
 		even though design and code files aren't supposed to be together.
 	'''
-	
+
 	page = '''<!DOCTYPE HTML>'''
 	page += '''<html>
 		<head>
 			<title>webapp</title>
 		</head>
 
-		<body>			
+		<body>
 			<h3>CRIMR Phase_2 - imhoffc, earleyg, walkere</h3>
-			
+
 			<h4>Search</h4>
 			<p> Type in a district (such as Tenderloin or Central):</p>
-			
+
 			<!-- form -->
 			<form action="webapp.py" method="get">
 				<p>Search District:<input type="text" name="search" value="%s" /></p>
@@ -91,7 +89,7 @@ def getPageAsHTML(searchString):
 
 			<!-- results get popped in here -->
 			%s
-			
+
 			<!-- links -->
 			<h4>Source Code</h4>
 			<p> <a href="showsource.py?source=webapp.py">webapp.py Source Code</a> </p>
@@ -102,17 +100,17 @@ def getPageAsHTML(searchString):
 		</body>
 		</html>
 		''' % (searchString, getSearchResultsAsHTML(searchString))
-		
+
 	return page
-		
+
 def getSearchResultsAsHTML(searchString):
 	''' Returns the search results form the PSQL database, formatted into an HTML String
 		Will be placed directly into the output String of 'getPageAsHTML(searchString)'
 	'''
-	
+
 	if searchString == '':
 		return 'Please input something to get started!'
-	
+
 	outputString = ''
 	try:
 		dataFetcher = CrimeDataFetcher()
@@ -123,12 +121,12 @@ def getSearchResultsAsHTML(searchString):
 			for row in outputTable:
 				outputString += '<tr> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> </tr>' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6])
 			outputString += '</table>'
-	
+
 		except Exception, e:
 			outputString +=  'Cursor error: %s' % e
 	except Exception, e:
 		outputString += 'Connection error: %s' % e
-	
+
 	return outputString
 
 if __name__ == '__main__':
