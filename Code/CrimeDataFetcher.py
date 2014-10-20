e1ffd3fb435ff54a8da214b1c13db1efd1aff271
Charlie Imhoff <imhoffc@carleton.edu>
2014-10-20 13:31:54 -0500

    first pass at HUGE variable search fetcher.
    definatly not functional yet, which is why it's on this here branch..

--- Code/CrimeDataFetcher.py
+++ Code/CrimeDataFetcher.py
@@ -24,7 +24,6 @@ class CrimeDataFetcher:
     def _getConnection(self):
         return psycopg2.connect(database='earleyg', user='earleyg', password='field799java')
 
-    #Deprecated. Execute cursor queries using cursor.execute(query, (formatStrParam, ))
     def cleanInput(self, str):
         ''' Removes any control characters that out HTML might be screwed up by
         '''
@@ -41,6 +40,101 @@ class CrimeDataFetcher:
                 rowData.append(cell)
             table.append(rowData)
         return table
+
+    #ACCESSING DATA by Many Parameters
+    def getCrimesForSearch(self, searchParams):
+    	'''
+    		Returns a table of crimes for a given, complex search
+    		The search is constructed from a python dictionary of search keys
+    		and search values (for example 'resolution':'None'). Because of the way
+    		parameters are passed in as a dictionary, that makes every search narrower
+    		optional.
+    		
+    		Search Keys & Expected Types:
+    			- 'search'		: String (will be bluntly searched against the table)
+    			- 'district'	: String
+    			- 'crime_id'	: String
+    			- 'category'	: String
+    			- 'day'			: String
+    			- 'resolution'	: String 
+    	'''
+        connection = self._getConnection()
+        if connection is not None:
+        	cursor = connection.cursor()
+        	
+        	#craft the perfect query
+        	query = 'SELECT * FROM crimes'
+        	queryHasWhere = False
+        	
+        	#for each possible parameter, check where it's present, 
+        	#and if it is, append an extra condition to the query 
+        	if searchParams['search'] is not None:
+        		search = self.cleanInput(searchParams['search'])
+        		if queryHasWhere:
+        			query += ' AND'
+        		else:
+        			query += ' WHERE'
+        			queryHasWhere = True
+        		#narrow with search
+        		longOr = ' (district LIKE [search] OR description LIKE [search] OR category LIKE [search] OR resolution LIKE [search])'
+        		query += longOr.replace('[search]',search)
+        		
+        	if searchParams['district'] is not None:
+        		district = self.cleanInput(searchParams['district'])
+        		if queryHasWhere:
+        			query += ' AND'
+        		else:
+        			query += ' WHERE'
+        			queryHasWhere = True
+        		query += ' district LIKE %s' % district
+        		
+        	if searchParams['crime_id'] is not None:
+        		crime_id = self.cleanInput(searchParams['crime_id'])
+        		if queryHasWhere:
+        			query += ' AND'
+        		else:
+        			query += ' WHERE'
+        			queryHasWhere = True
+        		query += ' crime_id=%s' % crime_id
+        	
+        	if searchParams['category'] is not None:
+        		category = self.cleanInput(searchParams['category'])
+        		if queryHasWhere:
+        			query += ' AND'
+        		else:
+        			query += ' WHERE'
+        			queryHasWhere = True
+        		query += ' category LIKE %s' % category
+        		
+        	if searchParams['resolution'] is not None:
+        		resolution = self.cleanInput(searchParams['resolution'])
+        		if queryHasWhere:
+        			query += ' AND'
+        		else:
+        			query += ' WHERE'
+        			queryHasWhere = True
+        		query += ' resolution LIKE %s' % resolution
+        		
+        	if searchParams['day'] is not None:
+        		day = self.cleanInput(searchParams['day'])
+        		if queryHasWhere:
+        			query += ' AND'
+        		else:
+        			query += ' WHERE'
+        			queryHasWhere = True
+        		query += ' day LIKE %s' % day
+        	
+        	query += ' ORDER BY crime_id DESC'
+        	cursor.execute(query)
+        	
+        	#construct 2d array
+        	table = self.createTableFromCursor(cursor)
+        	connection.close()
+        	return table
+        
+        #else
+        return [[]]
+        	
 
     #ACCESSING DATA
     def getAllCrimesFromDistrict(self, districtString):
