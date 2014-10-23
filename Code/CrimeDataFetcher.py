36f2a1bb06faa70586a70b7068eeb6da52132428
Charlie Imhoff <imhoffc@carleton.edu>
2014-10-23 15:11:20 -0500

    hotfixed so that unfound persons are treated as unresolved by getRandomCrime

--- Code/CrimeDataFetcher.py
+++ Code/CrimeDataFetcher.py
@@ -217,7 +217,7 @@ class CrimeDataFetcher:
 
             #Execute the query in a safe manner, taking advantage of .execute()'s format
             #str compatibility & helpful injection attack detection.
-            query = "SELECT * FROM crimes WHERE resolution ILIKE 'none' ORDER BY random() limit 1"
+            query = "SELECT * FROM crimes WHERE (resolution ILIKE 'none' OR resolution ILIKE 'unfound') ORDER BY random() limit 1"
             cursor.execute(query)
 
             #Construct a 2D array of all the information from the query
